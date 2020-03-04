8d4f77b508d6ba30aa4a3621152b47fdebaba15b
priyankajalal <priyankajalaldit@gmail.com>
2020-03-04 10:46:51 -0800

    jquery demo added

--- blog-app/src/app/shared-demo/service/portfolioService.ts
+++ blog-app/src/app/shared-demo/service/portfolioService.ts
@@ -1,16 +1,17 @@
-import {Injectable} from "@angular/core";
-import {Subject, Subscription} from "rxjs";
-import {filter, map} from "rxjs/operators";
-import {PortfolioEvent} from "../models/portfolioEvent";
+import { Injectable } from "@angular/core";
+import { Subject, Subscription } from "rxjs";
+import { filter, map } from "rxjs/operators";
+import { PortfolioEvent } from "../models/portfolioEvent";
 
 @Injectable()
-export class PortfolioService{
+export class PortfolioService {
   private subject$ = new Subject();
 
   emit(event: PortfolioEvent) {
     this.subject$.next(event);
   }
   on(eventName: string, action: any): Subscription {
+    
     return this.subject$.pipe(
       filter((e: PortfolioEvent) => e.name === eventName),
       map((e: PortfolioEvent) => e.value)
